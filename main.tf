762625a7a75da07fa464cb9b8965bfd349a9f452
Marcello Teodori <marcello.teodori@alfresco.com>
2019-11-26 21:01:33 +0000

    AAE-586 updated version to incubator/7.1.0.M6

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ data "helm_repository" "alfresco" {
 
 resource "helm_release" "alfresco-process-infrastructure" {
   name       = "aae"
-  repository = "${data.helm_repository.alfresco.url}"
+  repository = "${data.helm_repository.alfresco-incubator.url}"
   chart      = "alfresco-process-infrastructure"
-  version    = "7.1.0-M5"
+  version    = "7.1.0-M6"
 
   values = [<<EOF
 global:

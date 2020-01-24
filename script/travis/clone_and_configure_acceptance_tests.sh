dd86b121bc5bd309fc2bba49af18a241c8674ba6
Sohel Saiyed <sohel.saiyed@alfresco.com>
2020-01-24 13:38:46 +0000

    [AAE-941] - update script files permission and location (#6)
    * [AAE-941] - update script files permission and location

--- script/travis/clone_and_configure_acceptance_tests.sh
+++ script/travis/clone_and_configure_acceptance_tests.sh
@@ -4,4 +4,3 @@ git clone -b develop https://$GITHUB_TOKEN@github.com/Alfresco/alfresco-process-
 cd ./alfresco-process-acceptance-tests
 mkdir -p $HOME/.m2
 cp settings.xml $HOME/.m2
-travis_wait 30 mvn ${MAVEN_CLI_OPTS} dependency:resolve dependency:resolve-plugins

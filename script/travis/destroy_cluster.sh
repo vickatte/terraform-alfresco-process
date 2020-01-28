6b95d6dc02422601696737c46567e71a50fed273
Sohel Saiyed <sohel.saiyed@alfresco.com>
2020-01-28 14:03:34 +0000

    [AAE-1607] - encrypt slack token (#7)

--- script/travis/destroy_cluster.sh
+++ script/travis/destroy_cluster.sh
@@ -4,5 +4,6 @@ cd $PWD/examples/rancher-eks/
 export KUBECONFIG=/home/travis/build/Alfresco/terraform-alfresco-process/examples/rancher-eks/.terraform/kubeconfig
 export K8S_API_URL=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$TF_VAR_cluster_name\")].cluster.server}")
 terraform init
+terraform refresh
 helm ls --all --short | xargs -L1 helm delete --purge
 terraform destroy --force
